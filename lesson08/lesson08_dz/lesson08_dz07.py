40ee7b1d96d8d5ba96c94baf687c692c9c03b6a7
YuriiVasylchuk <y.vasylchuk@gmail.com>
2020-11-02 15:02:54 +0200

    Init

--- lesson08/lesson08_dz/lesson08_dz07.py
+++ lesson08/lesson08_dz/lesson08_dz07.py
@@ -14,19 +14,19 @@ class ComplexNumber:
         self.z = 'a + b * i'
 
     def __add__(self, other):
-        print(f'The sum of z1 and z2 equals')
+        print(f'Сумма z1 и z2 равна')
         return f'z = {self.a + other.a} + {self.b + other.b} * i'
 
     def __mul__(self, other):
-        print(f'The multiplication of z1 and z2 equals')
+        print(f'Произведение z1 и z2 равно')
         return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
 
     def __str__(self):
         return f'z = {self.a} + {self.b} * i'
 
 
-z_1 = ComplexNumber(5, -7)
-z_2 = ComplexNumber(1, 8)
+z_1 = ComplexNumber(1, -2)
+z_2 = ComplexNumber(3, 4)
 print(z_1)
 print(z_1 + z_2)
 print(z_1 * z_2)
